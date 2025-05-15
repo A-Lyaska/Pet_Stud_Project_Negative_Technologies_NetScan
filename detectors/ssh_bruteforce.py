e78619fe4f38ac4f286dcb1c118e3e345b2ca04d
a_lyaska <2710747@mail.ru>
2025-05-15 08:47:40 +0300

    2.0

---
+++ detectors/ssh_bruteforce.py
@@ -0,0 +1,38 @@
+import time
+import re
+from collections import defaultdict
+from utils.logger import log_attack
+
+class SSHBruteForceDetector:
+    def __init__(self, logfile="/var/log/auth.log", threshold=5, time_window=10):
+        self.logfile = logfile
+        self.threshold = threshold
+        self.time_window = time_window
+        self.attempts = defaultdict(list)
+        self.pattern = re.compile(r"Failed password.*from (\d+\.\d+\.\d+\.\d+)")
+
+    def run(self):
+        print("[*] Мониторинг SSH логов...")
+        with open(self.logfile, "r") as f:
+            # Перемещаемся в конец файла
+            f.seek(0, 2)
+            while True:
+                line = f.readline()
+                if not line:
+                    time.sleep(0.1)
+                    continue
+
+                match = self.pattern.search(line)
+                if match:
+                    ip = match.group(1)
+                    now = time.time()
+                    self.attempts[ip].append(now)
+
+                    # Очистка старых записей
+                    self.attempts[ip] = [
+                        ts for ts in self.attempts[ip] if now - ts <= self.time_window
+                    ]
+
+                    if len(self.attempts[ip]) >= self.threshold:
+                        log_attack("SSH Brute-force Detected", ip)
+                        self.attempts[ip].clear()
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+from threading import Thread
 from detectors.portscan import PortScanDetector
+from detectors.ssh_bruteforce import SSHBruteForceDetector
 from utils.logger import log_attack
 
 def main():
-    print("[*] Network Attack Detector запущен...")
-    detector = PortScanDetector()
-    try:
-        detector.run()
-    except KeyboardInterrupt:
-        print("\n[*] Остановка...")
+    print("[*] NT_NetScan запущен...")
+
+    portscan_detector = PortScanDetector(iface="ens33")
+    ssh_detector = SSHBruteForceDetector()
+
+    t1 = Thread(target=portscan_detector.run)
+    t2 = Thread(target=ssh_detector.run)
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
 
 if __name__ == "__main__":
     main()
