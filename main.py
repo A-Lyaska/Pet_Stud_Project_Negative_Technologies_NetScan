949217c7bc99d96262f5031e152168652ea89218
a_lyaska <2710747@mail.ru>
2025-05-15 09:22:39 +0300

    2.2

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+from threading import Thread
 from detectors.portscan import PortScanDetector
-from utils.logger import log_attack
 from detectors.ssh_bruteforce import SSHBruteForceDetector
 
 def main():
     print("[*] NT_NetScan запущен...")
-    detector = PortScanDetector()
-    ssh_brutefroce = SSHBruteForceDetector()
+
+    portscan_detector = PortScanDetector()
+    ssh_bruteforce_detector = SSHBruteForceDetector()
+
+    t1 = Thread(target=portscan_detector.run)
+    t2 = Thread(target=ssh_bruteforce_detector.run)
+
+    t1.start()
+    t2.start()
+
     try:
-        detector.run()
-        ssh_brutefroce.run()
+        t1.join()
+        t2.join()
     except KeyboardInterrupt:
         print("\n[*] Остановка...")
 
