a079560efbbadda5aa7dbf89d283057141164a3c
a_lyaska <2710747@mail.ru>
2025-05-24 11:07:48 +0300

    6.2

--- detectors/icmp_tunnel.py
+++ detectors/icmp_tunnel.py
@@ -1,34 +1,24 @@
-from scapy.all import sniff, IP, ICMP, Raw
+from scapy.all import sniff, IP, ICMP
 from utils.logger import log_attack
-from collections import defaultdict
-import time
 
-class IcmpTunnelDetector:
-    def __init__(self, iface="ens33", threshold=10, size_limit=100, window=10):
+class ICMPTunnelDetector:
+    def __init__(self, iface="ens33", size_threshold=200):
         self.iface = iface
-        self.threshold = threshold
-        self.size_limit = size_limit
-        self.window = window
-        self.icmp_log = defaultdict(list)
+        self.size_threshold = size_threshold
 
     def process_packet(self, packet):
-        if packet.haslayer(ICMP) and packet[ICMP].type == 8:
-            src = packet[IP].src
-            dst = packet[IP].dst
-            current_time = time.time()
+        if packet.haslayer(IP) and packet.haslayer(ICMP):
+            ip_layer = packet[IP]
+            icmp_layer = packet[ICMP]
 
-            self.icmp_log[src].append(current_time)
-            self.icmp_log[src] = [
-                t for t in self.icmp_log[src] if current_time - t <= self.window
-            ]
+            if icmp_layer.type == 8:
+                payload_size = len(icmp_layer.payload)
 
-            payload_size = 0
-            if packet.haslayer(Raw):
-                payload_size = len(packet[Raw].load)
-
-            if len(self.icmp_log[src]) > self.threshold or payload_size > self.size_limit:
-                log_attack(f"Подозрение на ICMP туннель {src} -> {dst} (count={len(self.icmp_log[src])}, size={payload_size})", src)
-                self.icmp_log[src].clear()
+                if payload_size > self.size_threshold:
+                    log_attack(
+                        f"Подозрение на ICMP-туннель (большой объём пакетов): {payload_size} bytes",
+                        ip_layer.src
+                    )
 
     def run(self):
         print("[*] Мониторинг ICMP-туннелей активен...")
